e5cb58302a5996163387165a61bf4331c2d8e8dc
Jack Hall <jack.hall95@hotmail.co.uk>
2018-01-25 19:38:48 +0000

    Syntax Improvements

--- BasicAuthCredGen.py
+++ BasicAuthCredGen.py
@@ -7,9 +7,9 @@ import base64
 parser = argparse.ArgumentParser(description="----------Program-Help-Page----------", formatter_class=RawTextHelpFormatter)
 
 #Inline Arguments
-parser.add_argument("-su", help="Specify a single username for generation. \n")
+parser.add_argument("-u", help="Specify a single username for generation. \n")
 parser.add_argument("-uf", help="Specify a file containing a list of usernames for generation.  \n")
-parser.add_argument("-sp", help="Specify a single password for generation. \n")
+parser.add_argument("-p", help="Specify a single password for generation. \n")
 parser.add_argument("-pf", help="Specify a file containing a list of usernames for generation.  \n")
 parser.add_argument("-o", help="Specify an output file. \n")
 parser.add_argument("-s", help="Suppress Terminal Output. \n", action="store_true")
@@ -19,13 +19,6 @@ args = parser.parse_args()
 Output = True
 Suppress = False
 
-#Checks validity of arguments
-if args.su  == None and args.uf == None:
-    print("Please Provide Valid Username(s).")
-    exit()
-if args.sp  == None and args.pf == None:
-    print("Please Provide Valid Password(s).")
-    exit()
 if args.o == None:
     Output = False
     print("No Output File Has Been Provided.  Output Will Only Display Within Terminal.")
@@ -33,11 +26,20 @@ if args.s == True:
     Suppress = True
     print("Terminal Output Has Been Suppressed.")
 
+def ArgumentCheck():
+    #Checks validity of arguments
+    if args.u  == None and args.uf == None:
+        print("Please Provide Valid Username(s).")
+        exit()
+    if args.p  == None and args.pf == None:
+        print("Please Provide Valid Password(s).")
+        exit()
+
 #Obtains the usernames for processing
 def ObtainUsername():
     Username = []
-    if args.su:
-        Username.append(str(args.su))
+    if args.u:
+        Username.append(str(args.u))
     if args.uf:
         if os.path.isfile(str(args.uf))==False:
             print("Invalid User File Specified.")
@@ -46,11 +48,12 @@ def ObtainUsername():
             for line in Userfile:
                 Username.append(line.strip("\n"))
     return Username
+
 #Obtains the passwords for processing
 def ObtainPasswords():
     Password = []
-    if args.sp:
-        Password.append(str(args.sp))
+    if args.p:
+        Password.append(str(args.p))
     if args.pf:
         if os.path.isfile(str(args.pf))==False:
             print("Invalid Password File Specified.")
@@ -74,8 +77,9 @@ def B64CredGenerator(Username, Password, Output, Suppress):
             if Suppress == False:
                 print(Encoded)
             Counter+=1
-    print(str(Counter)+" Credentials Generated.")
+    print(str(Counter)+" Credential(s) Generated.")
 
+ArgumentCheck()
 print("---------Initialising.---------")
 B64CredGenerator(ObtainUsername(), ObtainPasswords(), Output, Suppress)
 print("-----Generation-Completed.-----")
